1530dfd7efa64a3f676ccb855a4841c6cf2e5a3c
SuperPablo987 <63468752+SuperPablo987@users.noreply.github.com>
2023-07-28 09:45:17 -0600

    post structure

---
+++ src/components/PostListItem.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native';
+
+import { Post } from '@/types';
+
+type PostListItemProps = {
+    post: Post;
+};
+
+export default function PostListItem({ post }: PostListItemProps) {
+    return (
+        <Text>{post.content}</Text>
+    );
+}
