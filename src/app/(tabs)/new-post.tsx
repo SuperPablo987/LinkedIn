afe86367474690f0bcbecee8dc895bf4828c95f5
SuperPablo987 <63468752+SuperPablo987@users.noreply.github.com>
2023-08-11 12:23:25 -0600

    Image Picker

--- src/app/(tabs)/new-post.tsx
+++ src/app/(tabs)/new-post.tsx
@@ -1,11 +1,15 @@
-import { Pressable, StyleSheet, TextInput } from 'react-native';
+import { Pressable, StyleSheet, TextInput, Image } from 'react-native';
 import { useNavigation, useRouter } from 'expo-router';
 import { useLayoutEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { FontAwesome } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 import { Text, View } from '@/components/Themed';
 
 export default function NewPostScreen() {
   const [content, setContent] = useState('');
+  const [image, setImage] = useState<string | null>(null);
 
   const navigation = useNavigation();
   const router = useRouter();
@@ -15,6 +19,23 @@ export default function NewPostScreen() {
 
     router.push('/(tabs)/');
     setContent('');
+    setImage(null);
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      // aspect: [4, 3],
+      quality: 0.5,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
   };
 
   useLayoutEffect(() => {
@@ -36,6 +57,22 @@ export default function NewPostScreen() {
         style={styles.input} 
         multiline
       />
+
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+
+      <View style={styles.footer}>
+        <Pressable onPress={pickImage} style={styles.iconButton}>
+          <FontAwesome name="image" size={24} color="black"  />
+        </Pressable>
+
+        <Pressable style={styles.iconButton}>
+          <FontAwesome name="camera" size={24} color="black"  />
+        </Pressable>
+
+        <Pressable style={styles.iconButton}>
+        <MaterialCommunityIcons name="dots-horizontal" size={24} color="black" />
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -47,6 +84,11 @@ const styles = StyleSheet.create({
   },
   input: {
     fontSize: 18,
+  },
+  image: {
+    aspectRatio: 1,
+    marginTop: 'auto',
+    width: '100%',
   },
   title: {
     fontSize: 20,
@@ -64,5 +106,16 @@ const styles = StyleSheet.create({
   postButtonText: {
     color: 'white',
     fontWeight: '500',
+  },
+  // footer
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 'auto',
+  },
+  iconButton: {
+    backgroundColor: 'gainsboro',
+    borderRadius: 100,
+    padding: 15,
   },
 });
